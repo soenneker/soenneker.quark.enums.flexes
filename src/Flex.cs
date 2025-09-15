8f3fb6f045fe2aa43c3535a7a1438d20272289a3
Jake Soenneker <jake@soenneker.com>
2025-09-15 16:43:28 -0500

    Initial commit

---
+++ src/Flex.cs
@@ -0,0 +1,11 @@
+﻿using Intellenum;
+
+namespace Soenneker.Quark.Enums.Flexes;
+
+/// <summary>
+/// An HTML CSS in a .NET enumeration form
+/// </summary>
+[Intellenum<string>]
+public partial class Flex
+{
+}
---
+++ test/Soenneker.Quark.Enums.Flexes.Tests/FlexTests.cs
@@ -0,0 +1,13 @@
+﻿using Soenneker.Tests.Unit;
+using Xunit;
+
+namespace Soenneker.Quark.Enums.Flexes.Tests;
+
+public sealed class FlexTests : UnitTest
+{
+    [Fact]
+    public void Default()
+    {
+
+    }
+}
